3840fa5f3b6c1726a47a32b6799baa2f6415c84b
Pamplemousse <xav.maso@gmail.com>
2014-09-07 00:00:01 +0200

    fix debug and erreur

--- serveur.py
+++ serveur.py
@@ -1,15 +1,19 @@
+import logging
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from jeu import Jeu
 from joueur import Joueur
 
 app = Flask(__name__)
-
+app.debug = True
 
 jeu = Jeu()
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/partie-de-fou")
 def partie_de_fou():
@@ -20,7 +24,8 @@ def partie_de_fou():
 def creer_joueur():
     joueur = Joueur(request.form['nom_joueur'])
     jeu.ajoute_des_joueurs([joueur])
-    return redirect(url_for('partie-de-fou'))
+    return redirect('partie-de-fou')
+
 
 if __name__ == "__main__":
     app.run()
